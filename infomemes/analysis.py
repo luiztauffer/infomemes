376a2aca9bccaf7b20c11dc6ece768ef582661e2
luiztauffer <luiz_tauffer@hotmail.com>
2020-03-29 23:55:38 +0700

    update analysis

--- infomemes/analysis.py
+++ infomemes/analysis.py
@@ -3,45 +3,53 @@ from infomemes.utils import media_color_schema
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import json
-
-# from infomemes.analysis import survival_statistics
-# survival_statistics('sim_0.json')
+import copy
 
 
-def survival_statistics(sim):
+def read_sim_results(sim, step_filtered=0):
     """
     Basic analysis of a simulation.
 
     sim: simulation object or string with path to json file.
+    step_filtered: int
+        Produces a boolan array 'filtered' with True for all media that were
+        active at any step > step_filtered.
     """
-    survival_times = []
-    position_x = []
-    position_y = []
-    cov_diagonal = []
-    cov_xy = []
-    mpr = []
     if isinstance(sim, str):
         with open(sim, 'r') as f:
             sim = json.loads(f.read())
         # metadata
         duration = sim['metadata']['duration']
         media_reproduction_rate = sim['metadata']['media_reproduction_rate']
+        media_deactivation_rate = sim['metadata']['media_deactivation_rate']
         covariance_punishment = sim['metadata']['covariance_punishment']
         individuals_xy = np.array(sim['metadata']['individuals_xy'])
+        individual_renewal_rate = sim['metadata']['individual_renewal_rate']
+        individual_mui = sim['metadata']['individual_mui']
+        individual_mcr = sim['metadata']['individual_mcr']
+        max_reward = sim['metadata']['max_reward']
         # data
-        activated = list(sim['data']['activated'].values())
-        deactivated = list(sim['data']['deactivated'].values())
+        activated = np.array(list(sim['data']['activated'].values()))
+        deactivated = np.array(list(sim['data']['deactivated'].values()))
         active = np.array([t == duration for t in deactivated])
         survival_times = np.array(deactivated) - np.array(activated)
         position_x = np.array(list(sim['data']['position_x'].values()))
         position_y = np.array(list(sim['data']['position_y'].values()))
-        cov_x = list(sim['data']['cov_x'].values())
-        cov_y = list(sim['data']['cov_y'].values())
-        cov_diagonal = np.array(cov_x) + np.array(cov_y)
-        cov_xy = list(sim['data']['cov_xy'].values())
-        mpr = list(sim['data']['meme_production_rate'].values())
+        cov_x = np.array(list(sim['data']['cov_x'].values()))
+        cov_y = np.array(list(sim['data']['cov_y'].values()))
+        cov_diagonal = cov_x + cov_y
+        cov_xy = np.array(list(sim['data']['cov_xy'].values()))
+        mpr = np.array(list(sim['data']['meme_production_rate'].values()))
+        filtered = np.array([deact > step_filtered for deact in deactivated])
     else:
+        survival_times = []
+        position_x = []
+        position_y = []
+        cov_diagonal = []
+        cov_xy = []
+        mpr = []
         for m in sim.all_media:
             if m.active:
                 survival_times.append(sim.current_step - m.activated)
@@ -53,18 +61,165 @@ def survival_statistics(sim):
             cov_xy.append(m.cov[0, 1])
             mpr.append(m.meme_production_rate)
 
+    results = {
+        'duration': duration,
+        'media_reproduction_rate': media_reproduction_rate,
+        'media_deactivation_rate': media_deactivation_rate,
+        'max_reward': max_reward,
+        'covariance_punishment': covariance_punishment,
+        'individuals_xy': individuals_xy,
+        'individual_renewal_rate': individual_renewal_rate,
+        'individual_mui': individual_mui,
+        'individual_mcr': individual_mcr,
+        'activated': activated[filtered],
+        'deactivated': deactivated[filtered],
+        'active': active[filtered],
+        'survival_times': survival_times[filtered],
+        'position_x': position_x[filtered],
+        'position_y': position_y[filtered],
+        'cov_x': cov_x[filtered],
+        'cov_y': cov_y[filtered],
+        'cov_diagonal': cov_diagonal[filtered],
+        'cov_xy': cov_xy[filtered],
+        'meme_production_rate': mpr[filtered],
+        'step_filtered': filtered
+    }
+    return results
+
+
+def all_sims_summary(sims_list, step_filtered=0):
+    """
+
+    Parameters
+    ----------
+    sims_list: list
+        List of json files with simulation results
+    step_filtered: int
+        Produces a boolan array 'filtered' with True for all media that were
+        active at any step > step_filtered
+
+    Returns
+    -------
+    df_sims: Pandas DataFrame with results by simulation
+    df_media: Pandas DataFrame with results by media
+    df_clusters: Pandas DataFrame with results by cluster
+    """
+    # Organize Simulations DataFrame
+    colnames_sims = ['covariance_punishment', 'media_reproduction_rate', 'media_deactivation_rate',
+                     'individual_mui', 'individual_renewal_rate', 'individual_mcr', 'max_reward',
+                     'clusters_distances']
+    df_sims = pd.DataFrame(columns=colnames_sims)
+
+    # Organize Media DataFrame
+    colnames_media = ['simulation', 'activated', 'deactivated', 'position_x', 'position_y',
+                      'cov_x', 'cov_y', 'cov_xy', 'meme_production_rate', 'survival_time']
+    df_media = pd.DataFrame(columns=colnames_media)
+
+    # Organize Clusters DataFrame
+    colnames_clusters = ['simulation', 'n_members', 'center_of_mass']
+    df_clusters = pd.DataFrame(columns=colnames_clusters)
+
+    # Iterate over all simulations in list and populate DataFrames
+    for sim_id, sim in enumerate(sims_list):
+        sim_results = read_sim_results(sim, step_filtered=step_filtered)
+
+        # Update Media DataFrame
+        n_media = sim_results['activated'].shape[0]
+        sim_array = [sim_id] * n_media
+        df = pd.DataFrame(data=np.array([sim_array, sim_results['activated'], sim_results['deactivated'],
+                                         sim_results['position_x'], sim_results['position_y'],
+                                         sim_results['cov_x'], sim_results['cov_y'], sim_results['cov_xy'],
+                                         sim_results['meme_production_rate'], sim_results['survival_times']]).T,
+                          columns=colnames_media)
+        df_media = pd.concat([df_media, df], ignore_index=True)
+
+        # Cluster analysis
+        position_x = sim_results['position_x']
+        position_y = sim_results['position_y']
+        active = sim_results['active']
+        X = np.array([position_x[active], position_y[active]]).T
+        dbc, center_of_mass, n_members, clusters_distances = cluster_analysis(data_points=X)
+        n_clusters = n_members.shape[0]
+        sim_array = [sim_id] * n_clusters
+
+        # Update Clusters DataFrame
+        df = pd.DataFrame(columns=colnames_clusters)
+        for i in range(n_clusters):
+            aux = pd.DataFrame(data=[[sim_array[i], n_members[i], [center_of_mass[i]]]],
+                               columns=colnames_clusters)
+            df = pd.concat([df, aux])
+
+        df_clusters = pd.concat([df_clusters, df], ignore_index=True)
+
+        # Update Simulations DataFrame
+        df = pd.DataFrame(data=[[sim_results['covariance_punishment'], sim_results['media_reproduction_rate'],
+                                 sim_results['media_deactivation_rate'], sim_results['individual_mui'],
+                                 sim_results['individual_renewal_rate'], sim_results['individual_mcr'],
+                                 sim_results['max_reward'], clusters_distances]],
+                          columns=colnames_sims, index=[sim_id])
+        df_sims = pd.concat([df_sims, df])
+
+    return df_sims, df_media, df_clusters
+
+
+def cluster_analysis(data_points):
+    """
+    Automatic cluster analysis.
+
+    Parameters:
+    data_points: 2D numpy array
+        2D numpy array of dimmensions [n_points, 2] with (x, y) positions of n_points.
+    """
+    dbc = DBSCAN(eps=0.2, min_samples=3).fit(data_points)
+    cluster_labels = dbc.labels_
+    n_clusters = np.unique(cluster_labels).shape[0]
+    # Clusters center of mass and sizes
+    center_of_mass = np.zeros(shape=(n_clusters, 2))
+    clusters_sizes = np.zeros(n_clusters, dtype=int)
+    for cln in np.unique(cluster_labels):
+        ind = cluster_labels == cln
+        points = data_points[ind]
+        center_of_mass[cln, :] = np.mean(points, axis=0)
+        clusters_sizes[cln] = int(points.shape[0])
+    # Clusters distances
+    clusters_distances = []
+    for i, cm_i in enumerate(center_of_mass):
+        for j, cm_j in enumerate(center_of_mass[i + 1:, :]):
+            clusters_distances.append(np.sqrt((cm_i[0] - cm_j[0])**2 + (cm_i[1] - cm_j[1])**2))
+    clusters_distances = np.array(clusters_distances)
+
+    return (dbc, center_of_mass, clusters_sizes, clusters_distances)
+
+
+def survival_statistics(sim):
+    sim_results = read_sim_results(sim)
+
+    individual_renewal_rate = sim_results['individual_renewal_rate']
+    individual_mui = sim_results['individual_mui']
+    individuals_xy = sim_results['individuals_xy']
+    covariance_punishment = sim_results['covariance_punishment']
+    media_reproduction_rate = sim_results['media_reproduction_rate']
+    position_x = sim_results['position_x']
+    position_y = sim_results['position_y']
+    active = sim_results['active']
+    survival_times = sim_results['survival_times']
+    cov_xy = sim_results['cov_xy']
+    cov_diagonal = sim_results['cov_diagonal']
+    mpr = sim_results['mpr']
+
     max_time = max(survival_times)
     min_time = min(survival_times)
 
     # Cluster analysis
     X = np.array([position_x[active], position_y[active]]).T
-    db = DBSCAN(eps=0.2, min_samples=3).fit(X)
-    cluster_labels = db.labels_
+    dbc, center_of_mass, clusters_sizes, clusters_distances = cluster_analysis(data_points=X)
+    cluster_labels = dbc.labels_
     cluster_colors = [media_color_schema[i % len(media_color_schema)](1) for i in cluster_labels]
 
     # Plots
     fig = plt.figure()
-    fig.suptitle(f'MRR: {media_reproduction_rate}, CP:{covariance_punishment}', fontsize=16)
+    fig.suptitle(f'M_RR: {media_reproduction_rate}, CP:{covariance_punishment} \n'
+                 f'I_RR: {individual_renewal_rate}, I_MUI: {individual_mui}', fontsize=16)
     gs = fig.add_gridspec(3, 2)
     ax1 = fig.add_subplot(gs[0, 0])
     ax1.hist(survival_times)
@@ -127,17 +282,6 @@ def plot_media_contours(media, ax):
             Z[i, j] = media.mvg.pdf([x, y])
     ax.contourf(X, Y, Z, levels=3, cmap=media.cmap)  # , alpha=0.3)
     ax.plot(media.x, media.y, 'o', color=media.cmap(1), alpha=1, markersize=5)
-
-
-def cluster_analysis(positions_xy):
-    """
-    Automatic cluster analysis.
-
-    Parameters:
-    positions_xy: 2D numpy array
-        2D numpy array of dimmensions [n_points, 2] with (x, y) positions of n_points.
-    """
-    n_points = positions_xy.shape[0]
 
 
 if __name__ == '__main__':
